9889beaa884fa4c61b19386a97db69538c0e6f4b
Sandeep Sadanandan <sandeep.sadanandan@mapcase.de>
2016-07-11 22:59:54 +0200

    get request working with devise

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
+
+  before(:each) do
+    user = FactoryGirl.create(:user)
+    sign_in user
+  end
+
   describe "GET /categories" do
     it "works! (now write some real specs)" do
       get categories_path
--- spec/requests/tags_spec.rb
+++ spec/requests/tags_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Tags", type: :request do
+
+  before(:each) do
+    user = FactoryGirl.create(:user)
+    sign_in user
+  end
+
   describe "GET /tags" do
     it "works! (now write some real specs)" do
       get tags_path
