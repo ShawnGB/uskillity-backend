841af5011917d24bd114b56fcf91924865408ff1
Sandeep Sadanandan <sandeep.sadanandan@mapcase.de>
2016-07-11 20:14:48 +0200

    add validations to category name

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,8 @@ class Category < ApplicationRecord
   has_many :sub_categories, class_name: 'Category', foreign_key: :parent_id
 
   translates :name
+
+  validates :name_en, length: { within: 2..64 }
+  validates :name_de, length: { within: 2..64 }
+
 end
