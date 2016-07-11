54349d2402c308076c005ba7998dc12ef4494d08
Sandeep Sadanandan <sandeep.sadanandan@mapcase.de>
2016-07-11 22:08:14 +0200

    fine tune rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,14 @@
 # The `.rspec` file also contains a few flags that are not defaults but that
 # users commonly want.
 #
+
+# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start
+end
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
 	# Use color in STDOUT
