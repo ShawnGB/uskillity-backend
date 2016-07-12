882c65506fef55e44b7689b6fd2b1fc7dd6e2c9e
Sandeep Sadanandan <sandeep.sadanandan@mapcase.de>
2016-07-12 23:48:22 +0200

    clean up category views

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :ratings
   resources :levels
   resources :tags
-  resources :categories
+  resources :categories, only: [:index, :show]
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
