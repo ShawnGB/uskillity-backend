417400c8e41072c57e01b9f391b6802cc6011d23
Sandeep Sadanandan <sandeep.sadanandan@mapcase.de>
2016-07-12 09:32:22 +0200

    seed levels

--- app/models/level.rb
+++ app/models/level.rb
@@ -2,4 +2,6 @@ class Level < ApplicationRecord
   translates :name
 
   validates :value, inclusion: 0..100
+  validates :name_en, length: { within: 2..64 }
+  validates :name_de, length: { within: 2..64 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,11 @@ dance = Category.create(name_de: 'Tanzen', name_en: 'Dancing', parent: arts)
 Category.create(name_de: 'Salsa', name_en: 'Salsa', parent: dance)
 Category.create(name_de: 'Gitarre', name_en: 'Guitar', parent: arts)
 
+Level.create([
+  {value: 0, name_de: 'Neuling', name_en: 'Novice'},
+  {value: 20, name_de: 'Anfänger', name_en: 'Beginner'},
+  {value: 40, name_de: 'Fortgeschrittene Anfänger', name_en: 'Advanced Beginner'},
+  {value: 60, name_de: 'Kompetant', name_en: 'Knowledgeable'},
+  {value: 80, name_de: 'Expert', name_en: 'Expert'},
+  {value: 100, name_de: 'Guru', name_en: 'Guru'}
+])
