e8d70b5fcbbc10a34d171f6b1745639a5e8a45d3
Andreas Leicher <email@andreasleicher.com>
2016-07-15 12:04:38 +0200

    start user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'email is unique' do
+    subject { User.make }
+    # TODO decide if emails shall be unique
+    #it { expect(subject).to validate_uniqueness_of(:email) }
+  end
+
+  describe 'email validation' do
+    it { expect(subject).to allow_value('email@addresse.foo').for(:email) }
+    it { expect(subject).not_to allow_value('foo').for(:email) }
+  end
+
 end
