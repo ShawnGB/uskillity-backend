c8411ced4e54c58d2833f5c8813c4a8129bb5f5a
Andreas Leicher <email@andreasleicher.com>
2016-07-18 11:10:34 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  resources :participations
-  resources :workshop_registrations
-  resources :bookings
-  resources :workshop_sessions
-  resources :workshops
-  resources :venues
-  resources :comments
-  resources :ratings
-  resources :levels
-  resources :tags
+  resources :workshops do
+    resources :participations
+    resources :workshop_registrations, except: :delete
+    resources :bookings, except: :delete
+    resources :workshop_sessions do
+      resources :comments, only: [:create]
+      resources :ratings, only: [:create]
+    end
+  end
   resources :categories, only: [:index, :show]
+  resources :venues, only: [:index, :create]
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
