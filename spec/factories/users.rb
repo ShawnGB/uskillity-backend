73fc8282738da0860aea3073fb01eb95e88bc4aa
Andreas Leicher <email@andreasleicher.com>
2016-07-18 12:40:47 +0200

    style workshop overview

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,14 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| Faker::Internet.safe_email("person#{n}") }
     name  Faker::Name.last_name
+    first_name Faker::Name.first_name
     password Faker::Internet.password(10, 20)
+  end
+
+  factory :mike, class: User do
+    email "mike.michels@example.com"
+    name "Michels"
+    first_name "Mike"
+    password Faker::Internet.password(10,20)
   end
 end
