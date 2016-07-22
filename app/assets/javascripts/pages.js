dddea1486368527a32db32f5dca3d3ea9b827186
Andreas Leicher <email@andreasleicher.com>
2016-07-22 17:26:50 +0200

    add toggle for learn/offer content on landing page

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -5,4 +5,17 @@ $(document).ready(function() {
     paginationSpeed : 400,
     singleItem:true
   });
+
+  $('#switch-slides .btn').on('click', function() {
+    $('#switch-slides .btn').removeClass('active');
+    $(this).addClass('active');
+  });
+  $('#learn_click').on('click', function() {
+    $('#offer-slide').hide();
+    $('#learn-slide').show();
+  });
+  $('#offer_click').on('click', function() {
+    $('#learn-slide').hide();
+    $('#offer-slide').show();
+  });
 });
