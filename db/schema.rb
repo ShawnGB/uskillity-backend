14890d8f948a448bd8b77ba92c95d1ede5a21866
Andreas Leicher <email@andreasleicher.com>
2016-07-22 11:44:16 +0200

    add image to category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721092402) do
+ActiveRecord::Schema.define(version: 20160722094132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20160721092402) do
     t.hstore   "name_translations"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.string   "image"
   end
 
   create_table "comments", force: :cascade do |t|
