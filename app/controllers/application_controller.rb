0c13030696e38533f7ac6eab94f046666a72095a
Andreas Leicher <email@andreasleicher.com>
2016-07-22 13:30:10 +0200

    add footer with dynamic categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_filter :load_categories
   protect_from_forgery with: :exception, prepend: true
+
+  def load_categories
+    @categories = Category.all
+  end
 end
