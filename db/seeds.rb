ede55241dc4f4fcdd4d73cd2eb9b01660f405cda
Sandeep Sadanandan <sandeep.sadanandan@mapcase.de>
2016-10-05 10:36:02 +0200

    change the seeds to match the plan from Shawn

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-tech = Category.create(name_de: 'Technologie', name_en: 'Technology', image: '/assets/categories/technology.png')
-Category.create(name_de: 'Programmierung', name_en: 'Programming', parent: tech, image: '/assets/categories/programming.png')
+#tech = Category.create(name_de: 'Technologie', name_en: 'Technology', image: '/assets/categories/technology.png')
+#Category.create(name_de: 'Programmierung', name_en: 'Programming', parent: tech, image: '/assets/categories/programming.png')
 
-arts = Category.create(name_de: 'Kunst', name_en: 'Arts', image: '/assets/categories/pottery.png')
-dance = Category.create(name_de: 'Tanzen', name_en: 'Dancing', parent: arts, image: '/assets/categories/dancers.png')
-Category.create(name_de: 'Salsa', name_en: 'Salsa', parent: dance, image: '/assets/categories/latindancers.png')
-Category.create(name_de: 'Gitarre', name_en: 'Guitar', parent: arts, image: '/assets/categories/guitar.png')
+#arts = Category.create(name_de: 'Kunst', name_en: 'Arts', image: '/assets/categories/pottery.png')
+#dance = Category.create(name_de: 'Tanzen', name_en: 'Dancing', parent: arts, image: '/assets/categories/dancers.png')
+#Category.create(name_de: 'Salsa', name_en: 'Salsa', parent: dance, image: '/assets/categories/latindancers.png')
+#Category.create(name_de: 'Gitarre', name_en: 'Guitar', parent: arts, image: '/assets/categories/guitar.png')
+
+
+Category.create(name_en: 'Business', name_de: 'Das Unternehmen', image: '/assets/categories/business-1012761_1920.png')
+Category.create(name_en: 'Tech & Science', name_de: 'Technologie&Wissenschaft', image: '/assets/categories/robot-507811_1920.png')
+Category.create(name_en: 'Creative&Arts', name_de: 'Kreative und Kunst.', image: '/assets/categories/brush-1683134_1920.png')
+Category.create(name_en: 'Lifestyle', name_de: 'Lebensstil.', image: '/assets/categories/legs-434918_1280.png')
+Category.create(name_en: 'Kids', name_de: 'Kinder', image: '/assets/categories/kids.png')
+Category.create(name_en: 'Tutoring', name_de: 'Unterrichten', image: '/assets/categories/children-286239_1920.png')
+
 
 Level.create([
   {value: 0, name_de: 'Neuling', name_en: 'Novice'},
