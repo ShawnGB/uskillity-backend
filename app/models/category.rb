95e333e2b934d205da09fbb832967322fe00589a
Sandeep Sadanandan <sandeep.sadanandan@mapcase.de>
2016-10-07 20:28:47 +0200

    add the missing relationship

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :parent, class_name: 'Category'
   has_many :sub_categories, class_name: 'Category', foreign_key: :parent_id
+  has_many :workshops
 
   translates :name, :action_word
 
