29232577b85fcceaae7cf120e9d594fdfb3b8618
Sandeep Sadanandan <sandeep.sadanandan@mapcase.de>
2016-10-10 22:57:55 +0200

    make user account confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004090339) do
+ActiveRecord::Schema.define(version: 20161010205515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -131,6 +131,10 @@ ActiveRecord::Schema.define(version: 20161004090339) do
     t.string   "locale"
     t.string   "gender"
     t.string   "image"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
