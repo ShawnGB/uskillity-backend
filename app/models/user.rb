66e642f096bceca712503ac537a85e9825bed47e
Sandeep Sadanandan <sandeep.sadanandan@mapcase.de>
2016-10-10 22:46:52 +0200

    update devise to confirm the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   after_initialize :init
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
 
