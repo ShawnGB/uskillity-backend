6dbb866372883b1cb838d9a4024defc99fc93718
Sandeep Sadanandan <sillyfellow@whybenormal.org>
2018-01-05 14:33:52 +0100

    expose auth-headers explicitly (see: https://github.com/lynndylanhurley/devise_token_auth#cors )

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,10 @@ module BlueCarbuncle
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => :any
+        resource '*',
+          :headers => :any,
+          :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+          :methods => :any
       end
     end
 
