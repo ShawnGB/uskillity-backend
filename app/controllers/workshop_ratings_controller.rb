181e5c6f03093b92cd509322d80e47b7aa58b1f3
Sandeep Sadanandan <sillyfellow@whybenormal.org>
2018-01-30 21:50:45 +0100

    fix(auth): user should be authenticated to be able to rate something

--- app/controllers/workshop_ratings_controller.rb
+++ app/controllers/workshop_ratings_controller.rb
@@ -1,6 +1,6 @@
 class WorkshopRatingsController < ApplicationController
   before_action :set_rating, only: [:show, :edit, :update, :destroy]
-  #before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /workshops/:workshop_id/ratings
   # GET /workshops/:workshop_id/ratings.json
