8f2e4740b5aff190eef3df3ad28f441f211e976a
Sandeep Sadanandan <sillyfellow@whybenormal.org>
2018-02-01 16:05:19 +0100

    feat(serializers): for user and workshop, send image/images as the list of images (no need to send m

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
- attributes :id, :name, :first_name, :image, :nickname, :rating
- has_many :images
+  attributes :id, :name, :first_name, :nickname, :rating, :image
 
- def rating
-   rand(11) / 2.0
- end
+  def image
+    return  object.image if object.images.blank?
+    return object.images.sort_by{|image| image.id}.last.url.url
+  end
+
+  def rating
+    rand(11) / 2.0
+  end
 end
--- app/serializers/workshop_serializer.rb
+++ app/serializers/workshop_serializer.rb
@@ -1,7 +1,12 @@
 class WorkshopSerializer < ActiveModel::Serializer
   belongs_to :provider, class_name: 'User'
   attributes :id, :title, :subtitle, :description, :category_id, :offered_on, :fees, :provider_id, :main_image, :more_images, :recurrence_type, :is_approved, :minimum_workshop_registration_count, :maximum_workshop_registration_count, :created_at, :updated_at, :price, :level_id, :full_address, :min_age, :max_age, :published_at, :rating
-  has_many :images
+  attribute :images
+
+  def images
+    return (object.more_images || []).append(object.main_image).compact if object.images.blank?
+    return object.images.map{|image| image.url.url }
+  end
 
   def rating
     rand(11) / 2.0
