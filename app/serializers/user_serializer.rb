66ae6a9df26ad121d64169d6e7001471e42793f2
Sandeep Sadanandan <sillyfellow@whybenormal.org>
2018-02-01 13:30:15 +0100

    feat(userRating): send user rating (fake) in use rjson

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
- attributes :id, :name, :first_name, :image, :nickname
+ attributes :id, :name, :first_name, :image, :nickname, :rating
  has_many :images
+
+ def rating
+   rand(11) / 2.0
+ end
 end
