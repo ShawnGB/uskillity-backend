533662ef2641b082f1202c1a6b51aeb02221e109
Sandeep Sadanandan <sillyfellow@whybenormal.org>
2018-02-01 16:07:06 +0100

    feat(sessions): remove limitations from creating workshop sessions

--- app/models/workshop_session.rb
+++ app/models/workshop_session.rb
@@ -10,13 +10,13 @@ class WorkshopSession < ApplicationRecord
 
   translates :title, :subtitle, :description
 
-  validates :title_en, length: { within: 8..140 }
-  validates :title_de, length: { within: 8..140 }
-  validates :subtitle_en, length: { within: 8..140 }
-  validates :subtitle_de, length: { within: 8..140 }
-  validates :description_en, length: { within: 140..1024 }
-  validates :description_de, length: { within: 140..1024 }
+  #validates :title_en, length: { within: 8..140 }
+  #validates :title_de, length: { within: 8..140 }
+  #validates :subtitle_en, length: { within: 8..140 }
+  #validates :subtitle_de, length: { within: 8..140 }
+  #validates :description_en, length: { within: 140..1024 }
+  #validates :description_de, length: { within: 140..1024 }
 
-  validates :level, presence: true
-  validates :tutor, presence: true
+  #validates :level, presence: true
+  #validates :tutor, presence: true
 end
