156c4abc04329369519a7522115b33d206a618ae
Sandeep Sadanandan <sillyfellow@whybenormal.org>
2018-03-05 00:41:12 +0100

    embrace randomness

--- app/repository/workshop_repository.rb
+++ app/repository/workshop_repository.rb
@@ -2,7 +2,7 @@ class WorkshopRepository
 
   def self.random(count=6)
     imaged_ws_ids = Workshop.includes(:images).where.not(images: { of_id: nil }).pluck(:id)
-    workshops.where(id: imaged_ws_ids).take(count)
+    workshops.where(id: imaged_ws_ids).sample(count)
   end
 
   def self.categorised_workshops(category)
