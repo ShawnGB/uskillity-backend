ee4c3c7b48d54394df703cfa21c5102b91c329be
Andreas Leicher <email@andreasleicher.com>
2018-04-05 20:13:55 +0200

    feat(stripe): make sure only providers who can receive payments can create workshops
    also: change the api to convey to the user status that the user can receive payments

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -5,6 +5,10 @@ class UserSerializer < ActiveModel::Serializer
     return object.images.sort_by{|image| image.id}.last.url.url
   end
 
+  def stripe_provider
+    return object.can_receive_payments?
+  end
+
   def rating
     rand(11) / 2.0
   end
