b6684472e5ec90c1e3324578dae8f243412f6f5b
Sandeep Sadanandan <sillyfellow@whybenormal.org>
2018-04-30 10:27:10 +0200

    fix(participations): do not sent cancelled participations to the client

--- app/serializers/workshop_session_serializer.rb
+++ app/serializers/workshop_session_serializer.rb
@@ -1,3 +1,7 @@
 class WorkshopSessionSerializer < ActiveModel::Serializer
   attributes :id, :starts_at, :ends_at, :participations
+
+  def participations
+    return object.participations.select { |p| !p.is_cancelled }
+  end
 end
